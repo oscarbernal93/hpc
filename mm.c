9a613fd46a164d19606aeaa13b551fb959535221
Oscar <oscar@checkpoint>
2017-08-23 15:14:11 -0500

    defined static matrix size

--- mm.c
+++ mm.c
@@ -6,32 +6,21 @@
 int main()
 {
     int **a, **b, **r;
-    int ra, ca, rb, cb, i, j, k;
+    int i, j, k;
+    int s = SIZE;
 
-    printf("Size of first matrix: ");
-    scanf("%d %d", &ra, &ca);
-
-    printf("Size of second matrix: ");
-    scanf("%d %d",&rb, &cb);
-
-    if(ca != rb)
-    {
-	printf("Error! column of first matrix not equal to row of second one.\n");
-	return 0;
-    }
-
-    reserve_mat(&a,ra,ca);
-    reserve_mat(&b,rb,cb);
-    reserve_mat(&r,ra,cb);
+    reserve_mat(&a,s,s);
+    reserve_mat(&b,s,s);
+    reserve_mat(&r,s,s);
     
-    fill_mat(a ,ra ,ca);
-    fill_mat(b ,rb ,cb);
+    fill_mat(a ,s ,s);
+    fill_mat(b ,s ,s);
 
     clock_t begin = clock();
 
-    for(i=0; i<ra; ++i)
-        for(j=0; j<cb; ++j)
-            for(k=0; k<rb; ++k)
+    for(i=0; i<s; ++i)
+        for(j=0; j<s; ++j)
+            for(k=0; k<s; ++k)
             {
                 r[i][j]+=a[i][k]*b[k][j];
             }
@@ -39,9 +28,9 @@ int main()
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     
-    printf("Spent: %f\n",time_spent);
-    free_mat(a,ra);
-    free_mat(b,rb);
-    free_mat(r,ra);
+    printf("Time Spent: %f\n",time_spent);
+    free_mat(a,s);
+    free_mat(b,s);
+    free_mat(r,s);
     return 0;
 }
--- mmo.c
+++ mmo.c
@@ -7,39 +7,27 @@
 int main (int argc, char *argv[]) 
 {
 	int **a, **b, **c;
-	int ra, rb, ca, cb, tid, nt, id, i, j, k,chunk;
-	printf("Size of first matrix: ");
-	scanf("%d %d", &ra, &ca);
-	printf("Size of second matrix: ");
-	scanf("%d %d",&rb, &cb);
+	int tid, nt, id, i, j, k;
+	int s = SIZE, chunk = 10; 
 
-	if(ca != rb)
-	{
-		printf("Error! column of first matrix not equal to row of second one.\n");
-		return 0;
-	}
+	reserve_mat(&a,s,s);
+	reserve_mat(&b,s,s);
+	reserve_mat(&c,s,s);
 	
-	reserve_mat(&a,ra,ca);
-	reserve_mat(&b,rb,cb);
-	reserve_mat(&c,ra,cb);
-	
-	fill_mat(a ,ra ,ca);
-	fill_mat(b ,rb ,cb);	
-
-	printf("Size of chunk: ");
-	scanf("%d",&chunk);
+	fill_mat(a ,s ,s);
+	fill_mat(b ,s ,s);	
 	
 	clock_t begin = clock();
 	
-	#pragma omp parallel shared(a,b,c,ra, rb, ca, cb,nt,chunk) private(id,i,j,k)
+	#pragma omp parallel shared(a,b,c,s,nt,chunk) private(id,i,j,k)
 	{
 	//id = omp_get_thread_num();
 	//nt = omp_get_num_threads();
 
 	#pragma omp for schedule (static, chunk)
-	for(i=0; i<ra; ++i)
-		for(j=0; j<cb; ++j)
-		    for(k=0; k<rb; ++k)
+	for(i=0; i<s; ++i)
+		for(j=0; j<s; ++j)
+		    for(k=0; k<s; ++k)
 		    {
 		        c[i][j]+=a[i][k]*b[k][j];
 		    }
@@ -48,9 +36,9 @@ int main (int argc, char *argv[])
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 	
-	printf("Spent: %f\n",time_spent);
-	free_mat(a,ra);
-	free_mat(b,rb);
-	free_mat(c,ra);
+	printf("Time Spent: %f\n",time_spent);
+	free_mat(a,s);
+	free_mat(b,s);
+	free_mat(c,s);
 
 }
